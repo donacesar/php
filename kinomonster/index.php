955cf5e9ae205c0b3ed96d05170b08ad419a30cb
donacesar <donacesar@gmail.com>
2020-02-04 18:42:57 +0300

    lesson 3

---
+++ kinomonster/index.php
@@ -0,0 +1,35 @@
+<?php 
+
+	define("DBNAME", "kinomonster");
+	$nickname = "Sergey";
+	$balance = 0;
+	$addBalance1 = 10;
+	$addBalance2 = 35;
+
+	$balance = $addBalance1 + $addBalance2;
+	
+
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<div>Привет, <?php echo $nickname; ?></div>
+
+	<?php
+	 	echo "<div>".$balance."</div>";
+	 	echo DBNAME;
+	 ?>
+	
+</body>
+</html>
+
+
+
+
+
