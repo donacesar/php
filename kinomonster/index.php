e78e3ae97d1a12f0733482fd0887f11fe939c7a3
donacesar <donacesar@gmail.com>
2020-02-13 00:30:31 +0300

    lesson 39

--- kinomonster/index.php
+++ kinomonster/index.php
@@ -17,11 +17,44 @@ $i = 1;
 	echo $i."<br>";
 }*/
 
-$arr[0] = "продукты";
-$arr[1] = "бутылка с водой";
-for ($i=0; $i < count($arr); $i++) { 
-	echo $arr[$i]."<br>";
+/*$arr["product"] = "продукты";
+$arr["water"] = "бутылка с водой";*/
+
+/*for ($i=0; $i < count($arr); $i++) { 
+	echo $arr[$i]."<br>";*/
+
+/*$arr = array('produkt' => "продукты", "water" => "вода" );*/
+
+$arr = ["продукты", "вода"];
+
+/*unset($arr[0]);*/
+
+array_push($arr, "яблоко");
+array_unshift($arr, "апельсин");
+array_push($arr, "апельсин");
+
+$arr = array_unique($arr);
+// $arr = array_reverse($arr);
+
+/*sort($arr);*/
+/*shuffle($arr);
+*/
+$arr2 = array("кокос", "арбуз");
+
+$newarr = array_merge($arr, $arr2);
+
+foreach ($newarr as $key => $value) {
+	echo "ключ: ".$key." значение: ".$value."<br>";
 }
+/*$str = implode(", ", $newarr);
+echo $str;*/
+$str = "продукты, бутылка с водой, кокос";
+$strtoarr = explode(", ", $str);
+
+echo "<pre>";
+print_r($strtoarr);
+echo "</pre>";
+
 ?>
 
 <!DOCTYPE html>
