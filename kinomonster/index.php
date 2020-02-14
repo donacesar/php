94840356b7d7f19f65d4091dd124dd449ddb4527
donacesar <donacesar@gmail.com>
2020-02-14 13:58:31 +0300

    lesson 42

--- kinomonster/index.php
+++ kinomonster/index.php
@@ -51,9 +51,25 @@ echo $str;*/
 $str = "продукты, бутылка с водой, кокос";
 $strtoarr = explode(", ", $str);
 
+
+
+$camera["market1"]["row1"][0] = "продукты";
+$camera["market1"]["row1"][1] = "сумка";
+$camera["market2"]["row2"][0] = "апельсин";
+$camera["market2"]["row2"][1] = "фотоаппарат";
+$camera["market2"]["row2"][2] = "телефон";
+
 echo "<pre>";
-print_r($strtoarr);
+print_r($camera);
 echo "</pre>";
+foreach ($camera as $key => $value) {
+	foreach ($value as $key2 => $value2) {
+		foreach ($value2 as $key3 => $value3) {
+			echo $value3;
+		}
+		
+	}
+}
 
 ?>
 
