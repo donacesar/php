de36f89943c161be3db8f35c1b3e2221adc7e4b4
donacesar <donacesar@gmail.com>
2020-02-24 09:34:10 +0300

    lesson 55

--- kinomonster/index.php
+++ kinomonster/index.php
@@ -9,11 +9,22 @@
 
 	$mysqli->set_charset('utf8');
 
-	$query = $mysqli->query('SELECT * FROM movie');
+	/*$query = $mysqli->query('SELECT * FROM movie');
 
 	while ($row =mysqli_fetch_assoc($query)) {
 		echo $row['name'].$row['year']."<br>";
-	}
+	}*/
+
+	/*$query = "INSERT INTO movie VALUES(null, 'Безумный Макс', 'Описание фильма Безумнй Макс', '2015', Now())";
+	$mysqli->query($query);
+*/
+
+	/*$query = "UPDATE movie SET year = 1990 WHERE id = 3";
+	$mysqli->query($query);
+*/
+	$query = "DELETE FROM movie WHERE id = 3";
+	$mysqli->query($query);
+
 
 	$mysqli->close();
 	
